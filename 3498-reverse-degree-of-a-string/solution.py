9e703696e7db957f1d323ef113eedffde953d874
Anas Shaik <anasshaik382@gmail.com>
2026-09-20 09:50:56 +0530

    Update solution.py

--- 3498-reverse-degree-of-a-string/solution.py
+++ 3498-reverse-degree-of-a-string/solution.py
@@ -1,1 +1,11 @@
+class Solution:
+    def reverseDegree(self, s):
+        total = 0
 
+        for i, ch in enumerate(s):
+            reverse_position = 26 - (ord(ch) - ord('a'))
+            string_position = i + 1
+
+            total += reverse_position * string_position
+
+        return total
